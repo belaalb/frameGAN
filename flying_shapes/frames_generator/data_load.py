9d8896debf859406a7c901bdb7bb123e23f5619a
IsabelaAlbuquerque <isabelamcalbuquerque@gmail.com>
2018-07-26 17:16:45 -0400

    fix in flying shapes data loader frames indexing

--- flying_shapes/frames_generator/data_load.py
+++ flying_shapes/frames_generator/data_load.py
@@ -12,7 +12,7 @@ class Loader(Dataset):
 		self.hdf5_name = hdf5_name
 
 		open_file = h5py.File(self.hdf5_name, 'r')
-		self.length = len(open_file['data'])
+		self.length = len(open_file['./train.hdf'])
 		open_file.close()
 
 		self.open_file = None
@@ -21,12 +21,12 @@ class Loader(Dataset):
 
 		if not self.open_file: self.open_file = h5py.File(self.hdf5_name, 'r')
 
-		scene = torch.from_numpy(np.moveaxis(self.open_file['data'][index], -1, 0)).float()
+		scene = torch.from_numpy(np.moveaxis(self.open_file['./train.hdf'][index], -1, 0)).float()
 		idx = np.random.randint(scene.size(0))
-		img = scene[idx]
+		img = scene[:, :, :, idx]
 		img = (img - 0.5) / 0.5
 
-		return img.unsqueeze(0)
+		return img
 
 	def __len__(self):
 		return self.length
